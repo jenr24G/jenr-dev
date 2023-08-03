680b0e47b52ed2829fb1a87d3f81b8cdcbea073d
Jen Reiss <jenc.reiss24@gmail.com>
2023-08-03 12:20:21 -0500

    create slideshow!

--- server/main.ml
+++ server/main.ml
@@ -14,6 +14,13 @@ let () =
         Templates.Home.hello
         |> Dream.html);
 
+    Dream.get "/slideshow/:slideshow_id/:slide_num"
+        (fun request ->
+          let slideshow_id = Dream.param request "slideshow_id"               in
+          let slide_num    = Dream.param request "slide_num" |> int_of_string in
+          Templates.Home.slideshow slideshow_id slide_num ".png" 2
+        |> Dream.html);
+
     Dream.get "/static/**"
         (Dream.static "./static");
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "src/server/*.eml.html"
+    "templates/*.eml.html"
   ],
   theme: {
     extend: {},
